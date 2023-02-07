69370affc779e98a9fa6af4dc9899c72eb8cbf3f
Ananda Umamil <zweimach@zweimach.org>
2023-02-08 00:11:57 +0700

    Add highlights query

---
+++ queries/highlights.scm
@@ -0,0 +1,162 @@
+(identifier) @variable
+
+[
+  "const"
+  "enum"
+  "extern"
+  "fn"
+  "inline"
+  "struct"
+  "union"
+  "module"
+  "private"
+  "define"
+  "distinct"
+  "fault"
+  "defer"
+] @keyword
+
+"return" @keyword.return
+
+[
+  "while"
+  "for"
+] @repeat
+
+[
+  "if"
+  "else"
+] @conditional
+
+"import" @include
+
+[
+  "="
+
+  "-"
+  "*"
+  "/"
+  "+"
+  "%"
+  "*%"
+
+  "@"
+  "~"
+  "|"
+  "&"
+  "&&"
+  "^"
+  "<<"
+  ">>"
+
+  "<"
+  "<="
+  ">="
+  ">"
+  "=="
+  "!="
+
+  "!"
+  "&&"
+  "||"
+
+  "-="
+  "+="
+  "*="
+  "/="
+  "%="
+  "|="
+  "&="
+  "^="
+  ">>="
+  "<<="
+  "--"
+  "++"
+
+  ".."
+
+  "=>"
+] @operator
+
+(boolean_literal) @boolean
+
+[
+  "."
+  ";"
+  ":"
+  "::"
+  ","
+] @punctuation.delimiter
+
+"..." @punctuation.special
+
+[
+  "("
+  ")"
+  "["
+  "]"
+  "{"
+  "}"
+] @punctuation.bracket
+
+(string_literal) @string
+(escape_sequence) @string.escape
+
+(nil_literal) @constant.builtin
+(integer_literal) @number
+(float_literal) @number
+(char_literal) @character
+
+(field_expression field: (identifier) @property)
+(field_expression value: (identifier) @type
+  (#match? @type "^_?[A-Z]"))
+
+(module_declaration (identifier) @namespace)
+(module_declaration (scoped_identifier (identifier) @namespace .))
+
+(import_declaration (identifier) @namespace)
+(import_declaration (scoped_identifier (identifier) @namespace .))
+
+(scoped_identifier
+  path: (identifier) @namespace)
+(scoped_identifier
+  (scoped_identifier
+    name: (identifier) @namespace))
+(scoped_type_identifier
+  path: (identifier) @namespace)
+(scoped_type_identifier
+  (scoped_identifier
+    name: (identifier) @namespace))
+
+[
+ (type_identifier)
+ (primitive_type)
+ (pointer_type)
+ (failable_type)
+ (array_type)
+] @type
+
+(const_declaration (identifier) @constant)
+(enumerator
+  name: (const_identifier) @constant)
+
+(call_expression
+  function: (identifier) @function)
+(call_expression
+  function: (scoped_identifier (identifier) @function .))
+(call_expression
+  function: (field_expression
+    field: (identifier) @function))
+(call_expression
+  function: (field_expression
+    field: (identifier) @function))
+(function_declaration
+  name: (identifier) @function)
+
+(comment) @comment
+
+(parameter (identifier) @parameter)
+
+(attribute (identifier) @attribute)
+
+(ERROR) @error
